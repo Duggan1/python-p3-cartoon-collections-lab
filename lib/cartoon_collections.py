1750000e82e83c5e30b412a367453b2e9ce78d6b
Kevin Duggan <kevinduggan95@gmail.com>
2023-03-14 16:23:33 -0500

    finished lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarfs):
+    count = 1
+    for dwarf in dwarfs:
+        print(f"{count}. {dwarf}")
+        count += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(oldL):
+    newL = []
+    for word in oldL:
+        newS = f"{word.capitalize()}!"
+        newL.append(newS)
+    return newL
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(daList):
+    for daWord in daList:
+        if len(daWord) > 4:
+            return True
+    return False 
 
-def find_the_cheese():
-    pass
+def find_the_cheese(daFood):
+    daCheeses = ["cheddar", "gouda", "camembert"]
+
+    for item in daFood:
+        if item in daCheeses:
+            return item 
